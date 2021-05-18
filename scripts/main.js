2ad57d200e9e0aeb39c9ac76f44d0d21ccbe7ec7
Igor Romanov <Hei155@yandex.ru>
2021-05-18 16:34:50 +0300

    refactoring

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,26 @@
 let popup = document.querySelector('.popup')
 let openPopup = document.querySelector('.menu__edit');
 let closePopup = document.querySelector('.popup__close');
-let pageOpacity = document.querySelector('.page');
-function buttonToggle() {
-    popup.classList.toggle('popup_toggle');
-    inputName.value = `${nameIn.textContent}`;
-    inputDescription.value = `${description.textContent}`;
-}
-
+let form = document.querySelector('.profle__info');
 let nameIn = document.querySelector('.menu__name');
 let description = document.querySelector('.menu__description');
 let inputName = document.querySelector('#name');
 let inputDescription = document.querySelector('#description')
-let save = document.querySelector('.popup__button');
-function saveChanges() {
+
+function buttonToggle() {
+    popup.classList.toggle('popup_toggle');
+    if (!popup.classList.contains('popup_toggle')) {
+        inputName.value = `${nameIn.textContent}`;
+        inputDescription.value = `${description.textContent}`;
+    }
+}
+function saveChanges(event) {
+    event.preventDefault();
     nameIn.textContent = `${inputName.value}`;
     description.textContent = `${inputDescription.value}`;
     buttonToggle();
 }
 
-let activeButton = document.querySelectorAll('.grid__like');
-for (let i = 0; i <= activeButton.length - 1; i+=1) {
-    activeButton[i].addEventListener('click', function(){
-        if (activeButton[i].hasAttribute('active')) {
-            activeButton[i].setAttribute('src', 'images/Like.svg')
-            activeButton[i].removeAttribute('active')
-        }
-        else  {
-            activeButton[i].setAttribute('src', 'images/buttonActive.svg')
-            activeButton[i].setAttribute('active', 'like_active')
-        }
-    })
-};
 closePopup.addEventListener('click', buttonToggle);
 openPopup.addEventListener('click', buttonToggle);
-save.addEventListener('click', saveChanges);
-
+form.addEventListener('submit', saveChanges);
